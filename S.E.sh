d0f57304ab3d49afb0e5a5466e59e4f8a99dc5f5
AS7ABELMGRA <79116899+AS7ABELMGRA@users.noreply.github.com>
2021-05-26 03:03:07 +0200

    Create S.E.sh

---
+++ S.E.sh
@@ -0,0 +1,29 @@
+#!/bin/python
+# 
+#
+# Name	:	ELMORSY ATTACK2
+# Code		:	python
+# Telgram : @ANONEMOS_SE
+# WhatsApp  :+201021597142
+# Insta     : 8elm_orsy8
+# github	:github.com/AS7ABELMGRA.
+#
+#
+#	℘S.E℘ ELMORSY.....¿
+#
+#  The thief will cost him a fine. Greetings, Mursi
+#
+#	℘S.E℘ SECRTER EBLS....¿
+#
+#
+#
+apt update
+apt upgrade 
+apt install figlet -y
+apt install toilet -y
+apt install nano -y
+apt install ruby -y
+apt install curl -y
+gem install lolcat
+clear
+toilet -f pagga "ELMORSY..." | lolcat
