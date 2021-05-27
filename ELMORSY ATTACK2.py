b809d8d6ca376f2a1a05c676d8189d7d2b6df2ea
AS7ABELMGRA <79116899+AS7ABELMGRA@users.noreply.github.com>
2021-05-27 13:16:24 +0200

    Update ELMORSY ATTACK2.py

--- ELMORSY ATTACK2.py
+++ ELMORSY ATTACK2.py
@@ -34,18 +34,18 @@ os.system("bash SECRTER EBLES.sh")
 print
 print
 print
-print "\033[1m\033[32m\t\t \033[1m\033[31m<==\033[33m[\033[32m Author Info \033[33m]\033[31m==>\033[0m"
+print "\033[1m\033[32m\t\t \033[1m\033[31m<==\033[33m[\033[32 Author Info \033[33m]\033[31m==>\033[0m"
 time.sleep(3)
 print
 print
 print
-print "\033[1m\033[33m\n\tDeveloper : \033[32m \033[33mS\033[32mutariya \033[33mP\033[32marixit\033[0m"
+print "\033[1m\033[33m\n\tDeveloper : \033[32m \033[33mS\033[32elmorsy \033[33mP\033[32elmorsy\033[0m"
 time.sleep(1)
-print "\033[1m\033[33m\n\tBlog      :  \033[32mhttp:\033[33m//\033[32mbhai\033[33m4\033[32myou.blogspot.com\033[0m"
+print "\033[1m\033[33m\n\tBlog      :  \033[32mhttp:\033[33m//\033[32mbhai\033[33m4\033[elmorsyscrterebles.blogspot.com\033[0m"
 time.sleep(1)
-print "\033[1m\033[33m\n\tGithub    :  \033[32mhttp:\033[33m//\033[32mwww.github.com\033[33m/\033[32mBhai4You\033[0m"
+print "\033[1m\033[33m\n\tGithub    :  \033[32mhttp:\033[33m//\033[32mwww.github.com\033[33m/\033[AS7ABELMGRA\033[0m"
 time.sleep(1)
-print "\033[1m\033[33m\n\tYoutube   : \033[32m \033[33mB\033[32mull \033[33mA\033[32mnonymous\033[0m"
+print "\033[1m\033[33m\n\tYoutube   : \033[32m \033[33mB\033[32mull \033[33mA\033[32AS7ABELMGRA\033[0m"
 time.sleep(2)
 print
 print
@@ -160,7 +160,7 @@ def plink():
 	print
 os.system("clear")
 print
-os.system( "bash logo.sh" )
+os.system( "bash SECRTER EBLES.sh")
 print
 def mnu():
 	print "\033[1m\033[32m[+]\033[36m Whois lookup"
@@ -180,7 +180,7 @@ print
 mnu()
 print
 print
-print "\033[1m\033[32mEnter Your IP Or Domain..."
+print "\033[1m\033[32mEnter Your IP Or Elmorsy..."
 print
 ip = raw_input("\033[1m\033[32m IP or Domain : \033[33m\033[1m")
 print
