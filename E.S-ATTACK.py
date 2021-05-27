7c406cb0a7c8e54fdd5c597d8ef5bc10edf339af
AS7ABELMGRA <79116899+AS7ABELMGRA@users.noreply.github.com>
2021-05-27 13:51:32 +0200

    Update E.S-ATTACK.py

--- E.S-ATTACK.py
+++ E.S-ATTACK.py
@@ -34,22 +34,6 @@ os.system("clear")
 os.system("bash SECRTEREBLES.sh")
 print
 print
-print
-print "\033[1m\033[32m\t\t \033[1m\033[31m<==\033[33m[\033[32 Author Info \033[33m]\033[31m==>\033[0m"
-time.sleep(3)
-print
-print
-print
-print "\033[1m\033[33m\n\tDeveloper : \033[32m \033[33mS\033[32elmorsy \033[33mP\033[32elmorsy\033[0m"
-time.sleep(1)
-print "\033[1m\033[33m\n\tBlog      :  \033[32mhttp:\033[33m//\033[32mbhai\033[33m4\033[elmorsyscrterebles.blogspot.com\033[0m"
-time.sleep(1)
-print "\033[1m\033[33m\n\tGithub    :  \033[32mhttp:\033[33m//\033[32mwww.github.com\033[33m/\033[AS7ABELMGRA\033[0m"
-time.sleep(1)
-print "\033[1m\033[33m\n\tYoutube   : \033[32m \033[33mB\033[32mull \033[33mA\033[32AS7ABELMGRA\033[0m"
-time.sleep(2)
-print
-print
 
 def ping():
 	print
